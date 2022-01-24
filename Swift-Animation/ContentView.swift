cf0d4db4d477adc30910ca754e07c0016c12bec5
Rion <rionfx.42@gmail.com>
2022-01-24 23:46:30 +0900

    First Commit

---
+++ Swift-Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-Animation
+//
+//  Created by FX on 2022/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Swift Animation")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-Animation/Swift_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_AnimationApp.swift
+//  Swift-Animation
+//
+//  Created by FX on 2022/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
